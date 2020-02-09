6bbea3564e1c576199ef528df2b910aff502fbf4
victorgvieira <vggvieira@gmail.com>
2020-02-09 20:36:23 -0300

    :sparkles: Build the Helo Rectangle App

---
+++ hello_rectangle/lib/main.dart
@@ -0,0 +1,43 @@
+// DONE Step 1.0: Delete the contents of your app's lib/main.dart.
+// DONE Step 1.1: Paste in the code from lib/main.dart in the HelloRectangle app into your app's lib/main.dart
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Rectangle',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello!',
+            style: TextStyle(fontSize: 40.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
+// DONE Step 1.2: Open your device or emulator, and build and launch the app using the green Play button in Android Studio or from the command line, do "flutter run" inside your app's project directory.
