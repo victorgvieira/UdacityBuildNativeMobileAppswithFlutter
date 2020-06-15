ffeea20a082c446a9a8114b8767b6bf1e262de5c
victorgvieira <vggvieira@gmail.com>
2020-06-15 01:10:13 -0300

    :hammer: Quiz: Add the Flutter Currency Converter API (attempt)

--- task_11_api/lib/api.dart
+++ task_11_api/lib/api.dart
@@ -2,7 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: Import relevant packages
+// DONE Step 1: Import relevant packages
+import 'dart:convert';
+import 'dart:io';
+
+import 'category.dart';
+import 'unit.dart';
 
 /// The REST API retrieves unit conversions for [Categories] that change.
 ///
@@ -13,20 +18,61 @@
 ///   GET /currency: get a list of currencies
 ///   GET /currency/convert: get conversion from one currency amount to another
 class Api {
+  // DONE Step 1.1: Add any relevant variables and helper functions
+  final httpClient = HttpClient();
+  final baseUrl = "flutter.udacity.com";
 
-  // TODO: Add any relevant variables and helper functions
+  Uri _getUnitsUri() {
+    return Uri.https(baseUrl, "/currency");
+  }
 
-  // TODO: Create getUnits()
+  Uri _getConverterUri() {
+    return Uri.https(baseUrl, "/currency/convert");
+  }
+
+// DONE Step 2: Create getUnits()
   /// Gets all the units and conversion rates for a given category.
   ///
   /// The `category` parameter is the name of the [Category] from which to
   /// retrieve units. We pass this into the query parameter in the API call.
   ///
   /// Returns a list. Returns null on error.
+  Future<List<Unit>> getUnits() async {
+    final httpRequest = await httpClient.getUrl(_getUnitsUri());
+    final httpResponse = await httpRequest.close();
+    // NOTE: HttpStatus.OK is deprecated
+    if (httpResponse.statusCode != HttpStatus.ok) {
+      return null;
+    }
+    final responseBody = await httpResponse.transform(utf8.decoder).join();
+    final jsonResponse = json.decode(responseBody);
+    final value = jsonResponse['units'];
+    return value;
+  }
 
-  // TODO: Create convert()
+// DONE Step 3: Create convert()
   /// Given two units, converts from one to another.
   ///
   /// Returns a double, which is the converted amount. Returns null on error.
-
+  Future<double> convert(
+    double amount,
+    String fromUnit,
+    String toUnit,
+  ) async {
+    final httpRequest = await httpClient.getUrl(_getConverterUri().replace(
+        queryParameters: {
+          "amount": amount.toString(),
+          "from": fromUnit,
+          "to": toUnit
+        }));
+    final httpResponse = await httpRequest.close();
+    // NOTE: HttpStatus.OK is deprecated
+    if (httpResponse.statusCode != HttpStatus.ok) {
+      return null;
+    }
+    final responseBody = await httpResponse.transform(utf8.decoder).join();
+    final jsonResponse = json.decode(responseBody);
+    final value = jsonResponse['conversion'].toDouble();
+    return value;
+  }
 }
